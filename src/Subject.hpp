c5327f6ff4c1ea8c41006ed277d44610201e4035
Tales Miranda <tales.miranda84@gmail.com>
2017-12-24 11:46:23 -0200

    Fix to subject interface

--- src/Subject.hpp
+++ src/Subject.hpp
@@ -33,24 +33,23 @@ public:
 
   static Subject create(unsigned w, unsigned h);
 
-  void clickDown(float x, float y, Button button);
-  void clickUp(float x, float y, Button button);
-  void drag(float x, float y);
-  void move(float x, float y);
+  bool clickDown(float x, float y, Button button);
+  bool clickUp(float x, float y, Button button);
+  bool move(float x, float y);
 
-  using Renderer = std::function<void(unsigned      w,
-                                      unsigned      h,
-                                      unsigned char bpp,
-                                      unsigned      linePadding,
-                                      void*         pixels)>;
+  using Renderer = std::function<void(unsigned             w,
+                                      unsigned             h,
+                                      unsigned char        bpp,
+                                      unsigned             stride,
+                                      const unsigned char* pixels)>;
   /**
    * Renders the resulting image
    */
-  void render(const Renderer& renderer);
+  void render(const Renderer& renderer) const;
 
   /**
    * Save with the internal format.
    */
-  void save(const char* path);
+  void save(const char* path) const;
 };
 }
